e4ee1158f27f61f5390553ac897a987382c751d2
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-04 13:07:36 +0700

    HCM_PHP_06_05_2024-13 Create JWT auth download and import jwt into laravel

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'user1';
+        $user->email = 'user1@gmail.com';
+        $user->password = Hash::make('123456');
+        $user->save();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+/**
+ * API Routes for Authentication
+ *
+ * This route group handles all authentication-related API endpoints.
+ * The group is prefixed with 'api/auth' and requires the 'api' middleware.
+ *
+ * Endpoints:
+ * - POST /api/auth/login: Handles user login and returns an access token.
+ * - GET /api/auth/profile: Retrieves the authenticated user's profile information.
+ * - POST /api/auth/logout: Logs out the authenticated user and invalidates the access token.
+ * - POST /api/auth/refresh: Refreshes the access token for the authenticated user.
+ *
+ * These endpoints use the `AuthController` to handle the corresponding logic.
+ *
+ * The following api with API routes bellow:
+ *
+ * 127.0.0.1/api/auth/{action}
+ * Example: 127.0.0.1/api/auth/login
+ */
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ()
+{
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+
 });
