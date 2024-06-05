e023e5bf1c50975b7187179dd3f218e1a6291749
tuan090932 <gietconheo88@gmail.com>
2024-06-05 09:44:52 +0700

    HCM_PHP_06_05_2024-12 Api Show All Books

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Books\BookController;
+use App\Http\Controllers\Api\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,10 +12,20 @@ use App\Http\Controllers\Api\Books\BookController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
-|
+|   
 */
 
-Route::get('/books', [BookController::class, 'index']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'books'
+
+], function () {
+    Route::get('/', [BookController::class, 'index']);
+});
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
