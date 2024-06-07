401ff418c183b2d756191975ecbd29c946b053d5
tuan090932 <gietconheo88@gmail.com>
2024-06-07 11:48:16 +0700

    HCM_PHP_06_05_2024-31 Add unique key for show all categories for admin

--- database/migrations/2024_06_07_030732_create_categories_table.php
+++ database/migrations/2024_06_07_030732_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id('category_id');
-            $table->string('category_name', 250);
+            $table->string('category_name', 250)->unique();
             $table->timestamps();
         });
     }
