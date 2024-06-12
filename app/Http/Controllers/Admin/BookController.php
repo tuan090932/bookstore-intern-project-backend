1f459bd91872130afa66e6e10391ac34cc3e2ea8
Trương Hữu Việt <hudanht2612@gmail.com>
2024-06-12 17:18:04 +0700

    HCM_PHP_06_05_2024-40 Create index languages for admin

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -20,7 +20,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        $books = Book::with(['authors', 'categories', 'languages', 'publishers'])->paginate(15);
+        $books = Book::with(['authors', 'categories', 'languages', 'publishers'])->get();
         // $books = Book::all();
 
         return view('admin.pages.books.index', compact('books'));
@@ -71,7 +71,7 @@ class BookController extends Controller
 
         return redirect()->route('books.index')->with('success', 'Book added successfully.');
     }
-
+    
 
     /**
      * Display the specified resource.
@@ -86,7 +86,7 @@ class BookController extends Controller
      */
     public function edit(string $id)
     {
-        $books = Book::findOrFail($id);
+        $books = Book::findOrFail($id);  
         $categories = Category::all();
         $authors = Author::all();
         $languages = Language::all();
@@ -98,30 +98,31 @@ class BookController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-    {   
-        $request->validate([
-            'title' => 'required|string|max:250',
-            'language_id' => 'required|integer',
-            'num_pages' => 'required|integer',
-            'publisher_id' => 'required|integer',
-            'category_id' => 'required|integer',
-            'image' => 'required|string|max:250|unique:books,image',
-            'description' => 'required|string|max:250',
-            'price' => 'required|numeric',
-            'stock' => 'required|integer',
-            'author_id' => 'required|integer',
-        ], [
-            'title.required' => 'Title is required.',
-            'language_id.required' => 'Language is required.',
-            'num_pages.required' => 'Number of pages is required.',
-            'publisher_id.required' => 'Publisher is required.',
-            'category_id.required' => 'Category is required.',
-            'image.required' => 'Image is required.',
-            'description.required' => 'Description is required.',
-            'price.required' => 'Price is required.',
-            'stock.required' => 'Stock is required.',
-            'author_id.required' => 'Author is required.',
-        ]);
+    {
+        try {
+            $request->validate([
+                'title' => 'required|string|max:250',
+                'language_id' => 'required|integer',
+                'num_pages' => 'required|integer',
+                'publisher_id' => 'required|integer',
+                'category_id' => 'required|integer',
+                'image' => 'required|string|max:250|unique:books,image,' . $id,
+                'description' => 'required|string|max:250',
+                'price' => 'required|numeric',
+                'stock' => 'required|integer',
+                'author_id' => 'required|integer',
+            ], [
+                'title.required' => 'Title is required.',
+                'language_id.required' => 'Language is required.',
+                'num_pages.required' => 'Number of pages is required.',
+                'publisher_id.required' => 'Publisher is required.',
+                'category_id.required' => 'Category is required.',
+                'image.required' => 'Image is required.',
+                'description.required' => 'Description is required.',
+                'price.required' => 'Price is required.',
+                'stock.required' => 'Stock is required.',
+                'author_id.required' => 'Author is required.',
+            ]);
 
             $book = Book::findOrFail($id);
             $book->update($request->all());
@@ -145,5 +146,5 @@ class BookController extends Controller
             return redirect()->route('books.index')->with('error', 'Failed to delete the book.');
         }
     }
-
+    
 }
