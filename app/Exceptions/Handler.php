d1795d626b4ebbfe8da1d2ef09cca59c1466b53c
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-12 17:28:24 +0700

    HCM_PHP_06_05_2024-36 Admin show all authors and pagination

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -57,19 +57,25 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof NotFoundHttpException && $request->is('api/*'))
+        if ($request->is('api/*') )
         {
-            return response()->json([
-                'error' => 'Not Found',
-            ], 404);
+            if ($exception instanceof NotFoundHttpException)
+            {
+                return response()->json([
+                    'error' => 'Not Found',
+                ], 404);
+            }
         }
 
-        if ($exception instanceof MethodNotAllowedHttpException && $request->is('api/*'))
+        if ($request->is('api/*'))
         {
-            return response()->json([
-                'error' => 'Method Not Allowed',
-                'message' => 'The ' . $request->method() . ' method is not supported for this route.'
-            ], 405);
+            if ($exception instanceof MethodNotAllowedHttpException)
+            {
+                return response()->json([
+                    'error' => 'Method Not Allowed',
+                    'message' => 'The ' . $request->method() . ' method is not supported for this route.'
+                ], 405);
+            }
         }
 
         return parent::render($request, $exception);
