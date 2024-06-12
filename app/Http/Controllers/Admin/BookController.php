3590a57c4019a8318dae3be100ef0779a1c0d46b
Trương Hữu Việt <hudanht2612@gmail.com>
2024-06-12 17:33:42 +0700

    HCM_PHP_06_05_2024-38 Create edit book function admin

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -98,30 +98,6 @@ class BookController extends Controller
     public function update(Request $request, string $id)
     {
         try {
-            $request->validate([
-                'title' => 'required|string|max:250',
-                'language_id' => 'required|integer',
-                'num_pages' => 'required|integer',
-                'publisher_id' => 'required|integer',
-                'category_id' => 'required|integer',
-                'image' => 'required|string|max:250|unique:books,image,' . $id,
-                'description' => 'required|string|max:250',
-                'price' => 'required|numeric',
-                'stock' => 'required|integer',
-                'author_id' => 'required|integer',
-            ], [
-                'title.required' => 'Title is required.',
-                'language_id.required' => 'Language is required.',
-                'num_pages.required' => 'Number of pages is required.',
-                'publisher_id.required' => 'Publisher is required.',
-                'category_id.required' => 'Category is required.',
-                'image.required' => 'Image is required.',
-                'description.required' => 'Description is required.',
-                'price.required' => 'Price is required.',
-                'stock.required' => 'Stock is required.',
-                'author_id.required' => 'Author is required.',
-            ]);
-
             $book = Book::findOrFail($id);
             $book->update($request->all());
             return redirect()->route('books.index')->with('success', 'Book updated successfully.');
