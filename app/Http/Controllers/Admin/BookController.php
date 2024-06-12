c6b16a81ce248d55b15d05c9cf5dccf11cf5ecc1
Trương Hữu Việt <hudanht2612@gmail.com>
2024-06-12 09:20:49 +0700

    HCM_PHP_06_05_2024-38 Create edit book function admin

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -20,7 +20,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        $books = Book::with(['author', 'category', 'language', 'publisher'])->get();
+        $books = Book::with(['authors', 'categories', 'languages', 'publishers'])->get();
         // $books = Book::all();
 
         return view('admin.pages.books.index', compact('books'));
@@ -98,7 +98,31 @@ class BookController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-    {
+    {   
+        $request->validate([
+            'title' => 'required|string|max:250',
+            'language_id' => 'required|integer',
+            'num_pages' => 'required|integer',
+            'publisher_id' => 'required|integer',
+            'category_id' => 'required|integer',
+            'image' => 'required|string|max:250|unique:books,image',
+            'description' => 'required|string|max:250',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'author_id' => 'required|integer',
+        ], [
+            'title.required' => 'Title is required.',
+            'language_id.required' => 'Language is required.',
+            'num_pages.required' => 'Number of pages is required.',
+            'publisher_id.required' => 'Publisher is required.',
+            'category_id.required' => 'Category is required.',
+            'image.required' => 'Image is required.',
+            'description.required' => 'Description is required.',
+            'price.required' => 'Price is required.',
+            'stock.required' => 'Stock is required.',
+            'author_id.required' => 'Author is required.',
+        ]);
+
         $books = Book::findOrFail($id);
         $books->update($request->all());
         return redirect()->route('books.index')->with('succes', 'Product updated successfully');
@@ -120,7 +144,7 @@ class BookController extends Controller
     public function search(Request $request)
     {
         $search = $request->input('search');
-        $books = Book::where('title', 'like', '%' . $search . '%')->with(['author', 'category', 'language', 'publisher'])->get();
+        $books = Book::where('title', 'like', '%' . $search . '%')->with(['authors', 'categories', 'languages', 'publishers'])->get();
         return view('admin.pages.books.index', compact('books'));
     }
 
