2d33098fef033fd0770906a6c92ae4e6af42d363
tuan090932 <gietconheo88@gmail.com>
2024-06-12 10:28:06 +0700

    HCM_PHP_06_05_2024-31 Show_all categories for admin fix conflict

--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -33,7 +33,7 @@
 
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="user_dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
                 <img class="img-profile rounded-circle" src="{{ asset('/assets/img/undraw_profile.svg') }}">
             </a>
