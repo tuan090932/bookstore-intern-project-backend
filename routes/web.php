e80d38cbce6f32d6239ad3c424f277e248595338
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-12 16:59:36 +0700

    HCM_PHP_06_05_2024_36_Admin_show_all_authors_and_pagination

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'indexPage'])->name('dashboard');
 
-Route::get('/', [DashboardController::class, 'indexPage'])->name('dashboard');
-
 Route::get('/login', function () {
     return view('admin.pages.auth.login');
 })->name('login');
