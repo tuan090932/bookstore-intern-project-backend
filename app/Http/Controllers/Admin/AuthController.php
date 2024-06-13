f7d7bdd3a2a832f043259441430e1bf2725cb918
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-13 10:29:48 +0700

    HCM_PHP_06_05_2024-55 Admin register user

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\AdminRegisterRequest;
 use App\Models\AdminUser;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -21,30 +23,28 @@ class AuthController extends Controller
     }
 
     /**
-     * Store a newly created admin in the database.
+     * Store a newly admin account in database.
      *
      * @param  \App\Http\Requests\AdminRegisterRequest  $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function store(AdminRegisterRequest $request)
     {
-        try {
-            // Create the admin
+        try
+        {
             AdminUser::create([
                 'admin_name' => $request->admin_name,
                 'admin_password' => Hash::make($request->admin_password),
                 'admin_email' => $request->admin_email,
             ]);
 
-            return redirect()->route('admin.register')->with('success', 'Admin account created successfully.');
+            return redirect()->route('admin.register')->with('success', 'Tạo tài khoản thành công.');
         }
-        catch (\Exception $e)
+        catch (Exception $e)
         {
-            // Log the error message if necessary
-            \Log::error('Error creating admin account: ' . $e->getMessage());
+            Log::error('Error creating admin account: ' . $e->getMessage());
 
-            // Redirect back with an error message
-            return redirect()->route('admin.register')->with('error', 'Failed to create admin account. Please try again.');
+            return redirect()->route('admin.register')->with('error', 'Tạo tài khoản thất bại. Vui lòng thử lại.');
         }
     }
 }
