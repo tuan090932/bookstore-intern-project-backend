0d2d8938a72eeec303b1e2ba9736bca067c6cdd5
Trương Hữu Việt <hudanht2612@gmail.com>
2024-06-13 15:53:59 +0700

    HCM_PHP_06_05_2024-40 Create index languages for admin

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,20 @@ Route::get('/forgot-password', function () {
     return view('admin.pages.auth.forgot-password');
 })->name('forgot-password');
 
-Route::resource('admin/users', UserController::class);
+Route::prefix('admin')->group(function(){
+    //Users
+    Route::resource('admin/users', UserController::class);
 
-Route::resource('admin/books', BookController::class);
-Route::get('admin/dashboard', [DashboardController::class, 'indexPage'])->name('dashboard');
-Route::get('admin/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
-Route::resource('admin/books', BookController::class);
-Route::resource('admin/languages', LanguageController::class);
+    //Dashboard
+    Route::get('/', [DashboardController::class, 'indexPage'])->name('dashboard');
 
-Route::get('/', [DashboardController::class, 'indexPage'])->name('dashboard');
+    //Books
+    Route::resource('admin/books', BookController::class);
+    Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
+
+    //Languages
+    Route::resource('admin/languages', LanguageController::class);
+});
+
+
 
