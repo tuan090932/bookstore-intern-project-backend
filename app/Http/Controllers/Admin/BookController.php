92c03272c65e77cd55ba09cca8412707877f5226
Trương Hữu Việt <hudanht2612@gmail.com>
2024-06-14 16:30:27 +0700

    HCM_PHP_06_05_2024-10 Admin create book

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -20,17 +20,22 @@ class BookController extends Controller
      */
     public function index()
     {
-        $books = Book::with(['authors', 'categories', 'languages', 'publishers'])->paginate(15);
+        $books = Book::with(['authors', 'categories', 'languages', 'publishers'])->get();
         // $books = Book::all();
 
         return view('admin.pages.books.index', compact('books'));
     }
+
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        $authors = Author::all();
+        $languages = Language::all();
+        $publishers = Publisher::all();
+        return view('admin.pages.books.create', compact('categories','authors', 'languages','publishers'));
     }
 
     /**
@@ -38,33 +43,12 @@ class BookController extends Controller
      */
     public function store(BookRequest $request)
     {
-        $request->validate([
-            'title' => 'required|string|max:250',
-            'language_id' => 'required|integer',
-            'num_pages' => 'required|integer',
-            'publisher_id' => 'required|integer',
-            'category_id' => 'required|integer',
-            'image' => 'required|string|max:250|unique:books,image',
-            'description' => 'required|string|max:250',
-            'price' => 'required|numeric',
-            'stock' => 'required|integer',
-            'author_id' => 'required|integer',
-        ], [
-            'title.required' => 'Title is required.',
-            'language_id.required' => 'Language is required.',
-            'num_pages.required' => 'Number of pages is required.',
-            'publisher_id.required' => 'Publisher is required.',
-            'category_id.required' => 'Category is required.',
-            'image.required' => 'Image is required.',
-            'description.required' => 'Description is required.',
-            'price.required' => 'Price is required.',
-            'stock.required' => 'Stock is required.',
-            'author_id.required' => 'Author is required.',
-        ]);
-
-        Book::create($request->all());
-
-        return redirect()->route('books.index')->with('success', 'Book added successfully.');
+        try{
+            Book::create($request->validated());
+            return redirect()->route('books.index')->with('success', 'Book added successfully.');
+        }catch(\Exception $e){
+            return redirect()->route('books.index')->with('error', 'Failed to add book.');
+        }
     }
 
     /**
@@ -80,12 +64,7 @@ class BookController extends Controller
      */
     public function edit(string $id)
     {
-        $books = Book::findOrFail($id);
-        $categories = Category::all();
-        $authors = Author::all();
-        $languages = Language::all();
-        $publishers = Publisher::all();
-        return view('admin.pages.books.edit', compact('books','categories','authors', 'languages','publishers'));
+        //
     }
 
     /**
@@ -93,33 +72,7 @@ class BookController extends Controller
      */
     public function update(BookRequest $request, string $id)
     {
-        $request->validate([
-            'title' => 'required|string|max:250',
-            'language_id' => 'required|integer',
-            'num_pages' => 'required|integer',
-            'publisher_id' => 'required|integer',
-            'category_id' => 'required|integer',
-            'image' => 'required|string|max:250|unique:books,image',
-            'description' => 'required|string|max:250',
-            'price' => 'required|numeric',
-            'stock' => 'required|integer',
-            'author_id' => 'required|integer',
-        ], [
-            'title.required' => 'Title is required.',
-            'language_id.required' => 'Language is required.',
-            'num_pages.required' => 'Number of pages is required.',
-            'publisher_id.required' => 'Publisher is required.',
-            'category_id.required' => 'Category is required.',
-            'image.required' => 'Image is required.',
-            'description.required' => 'Description is required.',
-            'price.required' => 'Price is required.',
-            'stock.required' => 'Stock is required.',
-            'author_id.required' => 'Author is required.',
-        ]);
-
-        $books = Book::findOrFail($id);
-        $books->update($request->all());
-        return redirect()->route('books.index')->with('succes', 'Product updated successfully');
+        //
     }
 
     /**
@@ -127,17 +80,7 @@ class BookController extends Controller
      */
    public function destroy(string $id)
     {
-       //
-    }
-
-    /**
-    * Search for books by title.
-    */
-    public function search(Request $request)
-    {
-        $search = $request->input('search');
-        $books = Book::where('title', 'like', '%' . $search . '%')->with(['authors', 'categories', 'languages', 'publishers'])->get();
-        return view('admin.pages.books.index', compact('books'));
+        //
     }
     
 }
