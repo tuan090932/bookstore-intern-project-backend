6c42b364f3c7878d8615c155b7c6012e7ba652a6
*f6Tl7nr3Os <phattt@hopee.com.vn>
2024-06-17 17:11:01 +0700

    HCM_PHP_06_05_2024-51 Build api update infor user for client - fix

--- database/migrations/2024_06_17_082710_make_phone_number_nullable_in_users_table.php
+++ database/migrations/2024_06_17_082710_make_phone_number_nullable_in_users_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_name')->nullable(false)->change();
             $table->string('phone_number')->nullable(false)->change();
         });
     }
