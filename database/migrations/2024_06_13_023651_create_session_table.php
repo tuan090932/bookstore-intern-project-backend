1b11e47fbd603cdf79dad0dc449572e6d3404cf3
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-17 10:57:20 +0700

    HCM_PHP_06_05_2024-37 Admin create author

---
+++ database/migrations/2024_06_13_023651_create_session_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('session', function (Blueprint $table)
+        {
+            $table->string('session_id', 250)->primary();
+            $table->unsignedBigInteger('admin_id');
+            $table->timestamp('expiration');
+            $table->timestamps();
+
+            $table->foreign('admin_id')->references('admin_id')->on('admin')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+}
