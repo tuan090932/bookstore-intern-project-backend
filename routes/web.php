1e4621bf85e07f1379adf50ecb2403ea9c165708
tuan090932 <gietconheo88@gmail.com>
2024-06-18 11:00:27 +0700

    HCM_PHP_06_05_2024-31 CRUD categories for admin

--- routes/web.php
+++ routes/web.php
@@ -56,10 +56,10 @@ Route::prefix('admin')->group(function () {
 
     Route::prefix('categories')->group(function () {
         Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
-        Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
-        Route::post('categories/store', [CategoryController::class, 'store'])->name('categories.store');
-        Route::get('categories/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
-        Route::put('categories/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
+        Route::post('/store', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::put('/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
         Route::delete('/destroy/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
     });
 });
