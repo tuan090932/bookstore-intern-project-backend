83438836d0314d2b33e836f4a274b05a8b8fb7c1
Trương Hữu Việt <hudanht2612@gmail.com>
2024-06-21 09:12:08 +0700

    HCM_PHP_06_05_2024-40 Create index languages for admin

--- app/Http/Controllers/Admin/LanguageController.php
+++ app/Http/Controllers/Admin/LanguageController.php
@@ -17,44 +17,4 @@ class LanguageController extends Controller
 
         return view('admin.pages.languages.index', compact('languages'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-       //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
