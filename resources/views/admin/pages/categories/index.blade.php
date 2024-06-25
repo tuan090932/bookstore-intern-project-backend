93458180d26e0f4e941062e7526fcd48cf85b498
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-25 15:03:33 +0700

    HCM_PHP_06_05_2024-73 Modify dashboard admin

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -25,9 +25,6 @@
     @endif
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Category List</h6>
-        </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="data_table" width="100%" cellspacing="0">
