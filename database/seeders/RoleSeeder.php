fe047a7a0776e342dc6cb176bf458765a309b256
*GAjaMY6Jhd <taihp@hopee.com.vn>
2024-06-25 11:34:22 +0700

    HCM_PHP_06_05_2024-67 Grant permission for admin

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['role_id' => 'ALL', 'role_name' => 'All permissions'],
+            ['role_id' => 'MG', 'role_name' => 'Manager'],
+            ['role_id' => 'READ', 'role_name' => 'Read only'],
+            ['role_id' => 'PUB', 'role_name' => 'Publishers'],
+            ['role_id' => 'AUTHO', 'role_name' => 'Authors'],
+            ['role_id' => 'BOOK', 'role_name' => 'Books'],
+            ['role_id' => 'CAT', 'role_name' => 'Categories'],
+            ['role_id' => 'ORD', 'role_name' => 'Orders'],
+            ['role_id' => 'CUST', 'role_name' => 'Customers'],
+        ];
+
+        foreach ($roles as $roleData) {
+            $role = new Role();
+            $role->role_id = $roleData['role_id'];
+            $role->role_name = $roleData['role_name'];
+            $role->save();
+        }
+    }
+}
