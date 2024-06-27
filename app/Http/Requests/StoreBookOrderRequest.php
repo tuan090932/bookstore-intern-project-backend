9c0b196db2b8b29dd70aeb342f20b825275199b8
*f6Tl7nr3Os <phattt@hopee.com.vn>
2024-06-27 15:39:48 +0700

    HCM_PHP_06_05_2024-81 Buil api order in BE

--- app/Http/Requests/StoreBookOrderRequest.php
+++ app/Http/Requests/StoreBookOrderRequest.php
@@ -32,29 +32,4 @@ class StoreBookOrderRequest extends FormRequest
             'books.*.price' => 'required|numeric|min:0',
         ];
     }
-
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [
-            'order_date.required' => __('validation.required', ['attribute' => 'order date']),
-            'order_date.date' => __('validation.date', ['attribute' => 'order date']),
-            'address_id.required' => __('validation.required', ['attribute' => 'address']),
-            'address_id.exists' => __('validation.exists', ['attribute' => 'address']),
-            'books.required' => __('validation.required', ['attribute' => 'books']),
-            'books.array' => __('validation.array', ['attribute' => 'books']),
-            'books.*.book_id.required' => __('validation.required', ['attribute' => 'book id']),
-            'books.*.book_id.exists' => __('validation.exists', ['attribute' => 'book id']),
-            'books.*.quantity.required' => __('validation.required', ['attribute' => 'quantity']),
-            'books.*.quantity.integer' => __('validation.integer', ['attribute' => 'quantity']),
-            'books.*.quantity.min' => __('validation.min', ['attribute' => 'quantity']),
-            'books.*.price.required' => __('validation.required', ['attribute' => 'price']),
-            'books.*.price.numeric' => __('validation.numeric', ['attribute' => 'price']),
-            'books.*.price.min' => __('validation.min', ['attribute' => 'price']),
-        ];
-    }
 }
